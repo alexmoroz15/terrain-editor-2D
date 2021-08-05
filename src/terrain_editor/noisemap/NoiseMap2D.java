275fc96bea463f0ff1629a057a1d17899f09d402
alexmoroz15 <aleksey.morozov@hotmail.com>
2021-08-05 10:19:26 -0700

    Add comments to NoiseMap2D.java

--- src/terrain_editor/noisemap/NoiseMap2D.java
+++ src/terrain_editor/noisemap/NoiseMap2D.java
@@ -3,12 +3,25 @@ package terrain_editor.noisemap;
 import java.util.ArrayList;
 import java.util.Random;
 
+/*
+Abstract Class that defines common functionality for all noise maps.
+ */
 public abstract class NoiseMap2D {
+    // RNG seed
     long seed;
+
+    // 512 pre-generated RNG values used for noise map
     int[] randomVals;
+
+    // How often the noise map pattern repeats horizontally and vertically.
+    // Smaller numbers mean the map is more "zoomed in"
     double frequency;
+
+    // Interpolation method used to retrieve intermediate values
     InterpMethod interp;
 
+    // Collection of functions that are called when one of the properties of
+    // the map has updated
     ArrayList<ChangeListener> changeListeners;
 
     public NoiseMap2D(long seed, double frequency) {
@@ -18,7 +31,9 @@ public abstract class NoiseMap2D {
         interp = new Lerp();
     }
 
-    public NoiseMap2D(int[] randomVals, double frequency, InterpMethod interp) {
+    // Internal constructor used to make "sub-maps" with the same pattern.
+    // Used by FractalMap2D.
+    protected NoiseMap2D(int[] randomVals, double frequency, InterpMethod interp) {
         changeListeners = new ArrayList<>();
         this.seed = 0;
         this.randomVals = randomVals;
@@ -26,6 +41,8 @@ public abstract class NoiseMap2D {
         this.interp = interp;
     }
 
+    // Returns a deterministic semi-random number that corresponds to the
+    // position components.
     public abstract double get(double x, double y);
 
     public void setSeed(long seed) {
@@ -37,6 +54,7 @@ public abstract class NoiseMap2D {
         }
         commitChanges();
     }
+
     public long getSeed() { return seed; }
 
     public void setFrequency(double frequency) {
@@ -52,6 +70,7 @@ public abstract class NoiseMap2D {
         changeListeners.add(cl);
     }
 
+    // Calls each of the change listeners. Used to report changes.
     protected void commitChanges() {
         for (var cl : changeListeners) {
             cl.handle(this);
@@ -62,10 +81,14 @@ public abstract class NoiseMap2D {
         void handle(NoiseMap2D newValue);
     }
 
+    // General interpolation method.
     public interface InterpMethod {
+        // y1 and y2 can be any number.
+        // t represents the distance from y1 to y2 in the range [0.0, 1.0].
         double interp(double y1, double y2, double t);
     }
 
+    // Standard linear interpolation.
     public static class Lerp implements InterpMethod {
         public double interp(double y1, double y2, double t) {
             return y1 * (1 - t) + y2 * t;
