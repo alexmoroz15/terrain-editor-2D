1c8a2e17d5c0909ad908e231c5d85ced0d63aa82
alexmoroz15 <aleksey.morozov@hotmail.com>
2021-08-05 15:16:12 -0700

    Add outline comment to Layer.java

--- src/terrain_editor/layer/Layer.java
+++ src/terrain_editor/layer/Layer.java
@@ -1,4 +1,18 @@
 package terrain_editor.layer;
 
+/*
+Represents a layer in the final, composite image.
+
+Each layer keeps track of:
+- its noise map
+- the control box for the noise map
+  - seed input
+  - frequency input
+  - lacunarity
+  - gain
+  - tiled sprite/image
+  - amplitude cutoffs for where to display image
+  - number of rows and columns
+ */
 public class Layer {
 }
